a2701089885274eddc5b388a9a8bd6740c6d0e0a
Vaibhav <vaibhavearande55555@gmail.com>
2023-11-14 09:30:00 +0530

    mern

---
+++ backend/data.js
@@ -0,0 +1,74 @@
+export default {
+    products: [
+      {
+        id: 10,
+        title: "SanDisk SSD PLUS 1TB Internal SSD  SATA III 6 Gbs",
+        price: 763,
+        description: "Easy upgrade for faster boot up shutdown application load and response As compared to 5400 RPM SATA 2.5 hard drive Based on published specifications and internal benchmarking tests using PCMark vantage scores Boosts burst write performance making it ideal for typical PC workloads The perfect balance of performance and reliability Readwrite speeds of up to 535MBs450MBs Based on internal testing Performance may vary depending upon drive capacity host device OS and application.",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        sold: false,
+        dateOfSale: "2022-03-27T20:29:54+05:30"
+    },
+    {
+        id: 11,
+        titl: "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 25",
+        price: 109,
+        description: "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultraslim notebooks. Supports TRIM command Garbage Collection technology RAID and ECC Error Checking  Correction to provide the optimized performance and enhanced reliability.",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
+        sold: false,
+        dateOfSale: "2021-09-27T20:29:54+05:30"
+    },
+    {
+        id: 12,
+        title: "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
+        price: 798,
+        description: "Expand your PS4 gaming experience Play anywhere Fast and easy setup Sleek design with high capacity 3year manufacturers limited warranty",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
+        sold: true,
+        dateOfSale: "2022-03-27T20:29:54+05:30"
+    },
+    {
+        id: 13,
+        title: "Acer SB220Q bi 215 inches Full HD 1920 x 1080 IPS UltraThin",
+        price: 4792,
+        description: "21. 5 inches Full HD 1920 x 1080 widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate 75Hz  Using HDMI port Zeroframe design  ultrathin  4ms response time  IPS panel Aspect ratio  16 9. Color Supported  16. 7 million colors. Brightness  250 nit Tilt angle 5 degree to 15 degree. Horizontal viewing angle178 degree. Vertical viewing angle178 degree 75 hertz",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+        sold: false,
+        dateOfSale: "2022-04-27T20:29:54+05:30"
+    },
+    {
+        id: 14,
+        title: "Samsung 49Inch CHG90 144Hz Curved Gaming Monitor LC49HG90DMNXZA  Super Ultrawide Screen QLED ",
+        price: 4999.95,
+        description: "49 INCH SUPER ULTRAWIDE 329 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT QLED TECHNOLOGY HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur ghosting and reduce input lag",
+        category: "electronics",
+        image: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
+        sold: true,
+        dateOfSale: "2022-01-27T20:29:54+05:30"
+    },
+    {
+        id: 15,
+        title: "BIYLACLESEN Womens 3in1 Snowboard Jacket Winter Coats",
+        price: 284.95,
+        description: "NoteThe Jackets is US standard size Please choose size as your usual wear Material 100 Polyester Detachable Liner Fabric Warm Fleece. Detachable Functional Liner Skin Friendly Lightweigt and Warm.Stand Collar Liner jacket keep you warm in cold weather. Zippered Pockets 2 Zippered Hand Pockets 2 Zippered Pockets on Chest enough to keep cards or keysand 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and waterfor a comfortable fit. 3 in 1 Detachable Design provide more convenience you can separate the coat and inner as needed or wear it together. It is suitable for different season and help you adapt to different climates",
+        category: "women's clothing",
+        image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+        sold: true,
+        dateOfSale: "2022-01-27T20:29:54+05:30"
+    },
+    {
+        id: 16,
+        title: "Lock and Love Womens Removable Hooded Faux Leather Moto Biker Jacket",
+        price: 59.9,
+        description: "100 POLYURETHANEshell 100 POLYESTERlining 75 POLYESTER 25 COTTON SWEATER Faux leather material for style and comfort  2 pockets of front 2ForOne Hooded denim style faux leather jacket Button detail on waist  Detail stitching at sides HAND WASH ONLY  DO NOT BLEACH  LINE DRY  DO NOT IRON",
+        category: "women's clothing",
+        image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
+        sold: false,
+        dateOfSale: "2021-10-27T20:29:54+05:30"
+      },
+    ]
+  }
